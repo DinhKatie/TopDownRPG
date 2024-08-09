d9a75ea300b0f1a652704ac05c9f8dc92f3b95d2
DinhKatie <87164647+DinhKatie@users.noreply.github.com>
2024-08-08 21:24:24 -0700

    Portal
    Create empty sprite for portal object, add boxCollider2D and Portal script inheriting from Collidable
    In script: check for Player collider, then load a random dungeon name from an array using Scene Manager
    In File -> Build Settings, add Dungeon 1 to the main game

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,18 @@
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal : Collidable
+{
+    public string[] sceneNames;
+
+    protected override void OnCollide(Collider2D collider)
+    {
+        if (collider.name == "Player")
+        {
+            //Teleport the player to random dungeon
+            string sceneName = sceneNames[Random.Range(0, sceneNames.Length)];
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
