b4cdb557b151caeb296ef160d1d04317df6f6510
DinhKatie <87164647+DinhKatie@users.noreply.github.com>
2024-08-15 18:42:16 -0700

    Add an extra layer for Moving
    Edit Player things

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,47 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : Fighter
+public class Player : Mover
 {
-    private BoxCollider2D boxCollider;
-    private Vector3 moveDelta;
-    private RaycastHit2D hit;
-
-    private void Start()
-    {
-        boxCollider = GetComponent<BoxCollider2D>();
-    }
-
-    //To sync with the physics engine
     private void FixedUpdate()
     {
         float x = Input.GetAxisRaw("Horizontal"); //GetAxisRaw returns an immediate 1, 0, -1, with no smoothing
         float y = Input.GetAxisRaw("Vertical");
 
-        //Reset moveDelta
-        moveDelta = new Vector3(x, y, 0);
-
-        //Swap sprite direction according to movement
-        if (moveDelta.x > 0)
-            transform.localScale = Vector3.one;
-        else if (moveDelta.x < 0)
-            transform.localScale = new Vector3(-1,1,1);
-
-        //Check for collision along the y-axis by casting a box there first. If box returns null, free to move
-        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(0, moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
-        if (hit.collider == null)
-        {
-            //Apply movement
-            transform.Translate(0, moveDelta.y * Time.deltaTime, 0);
-        }
-
-        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(moveDelta.x, 0), Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
-        if (hit.collider == null)
-        {
-            //Apply movement
-            transform.Translate(moveDelta.x * Time.deltaTime, 0, 0);
-        }
-
+        UpdateMotor(new Vector3(x, y, 0));
 
     }
 }
